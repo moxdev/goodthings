b487a193a3f5032ac130d417a495b8a69c8cccb9
moxdev <moxdev1@gmail.com>
2015-11-19 15:26:50 -0500

    added user controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get 'terms' => 'static_pages#terms'
   get 'contact' => 'static_pages#contact'
 
+  get 'signup' => 'users#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
