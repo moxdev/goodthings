f018c09d09ada5730358b47bc37dc431eb0f70e8
moxdev <moxdev1@gmail.com>
2015-11-19 16:32:03 -0500

    removed users controller

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test "full title helper" do
+    assert_equal full_title,         "Good Things"
+    assert_equal full_title("Help"), "Help | Good Things"
+    assert_equal full_title("About"), "About | Good Things"
+    assert_equal full_title("Contact"), "Contact | Good Things"
+    assert_equal full_title("Signup"), "Signup | Good Things"
+    assert_equal full_title("Developer"), "Developer | Good Things"
+    assert_equal full_title("Terms"), "Terms | Good Things"
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,8 @@ require "minitest/reporters"
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  # Added to test application_helper_test.rb
+  include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
 end
