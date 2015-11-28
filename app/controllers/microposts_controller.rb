6685f814ef2e6062017d2f1440fbb4ec94325eb8
moxdev <moxdev1@gmail.com>
2015-11-27 21:59:35 -0500

    setup home feed

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,8 +1,28 @@
 class MicropostsController < ApplicationController
+    before_action :authenticate_user!, only: [:index, :create, :destroy]
+
+    def index
+        @micropost = Micropost.paginate(page: params[:page])
+    end
+
     def create
+        @createpost = current_user.microposts.build(micropost_params)
+        if @createpost.save
+            flash[:success] = "Post successfully created!"
+            redirect_to profile_path
+        else
+            flash[:error] = "Post was not created!"
+            render profile_path
+        end
     end
 
     def destroy
+    end
+
+    private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
     end
 
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,14 @@
 class StaticPagesController < ApplicationController
-  skip_before_filter :authenticate_user!
+  skip_before_filter :authenticate_user!, #only:[:about]
   # limit actions like this - "only: [:home, :about]"
 
   def home
+    if user_signed_in?
+      redirect_to microposts_path
+    else
+      flash[:notice] = "Please login to see your home feed!"
+      root_path
+    end
   end
 
   def about
